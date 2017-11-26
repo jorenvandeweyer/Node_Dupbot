662c3517acd7da890f0be0a711360c3e66ca5533
Joren Vandeweyer <joren@Joren.local>
2017-11-26 04:15:35 +0100

    prefix command

---
+++ src/commands/prefix.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "prefix",
+    description: "!ping",
+    defaultPermission: 1,
+    args: 0,
+    execute(self, msg){
+        self.db.getSettings(msg.guild.id, "prefix", (pref) =>  {
+            let prefix = pref;
+            if(prefix == "") prefix = self.serverManager().prefix;
+            message = self.createEmbed("info", "The prefix is `" + prefix + "`");
+            self.send(msg, message);
+        });
+    }
+};
