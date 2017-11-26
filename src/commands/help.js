a8ab72e1842d6ae46c307c7e4577393edb846055
Joren Vandeweyer <joren@Joren.local>
2017-11-26 03:37:47 +0100

    un hardcocded help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,40 +1,43 @@
-const {prefix} = require("../../serverSettings.json");
-
 module.exports = {
     name: "help",
     description: "!help <command>",
     defaultPermission: 1,
     args: 0,
     execute(self, msg){
-    	if (msg.params.length >= 1){
-    		let helpmsg = "No command";
-            let command = msg.client.commands.get(msg.params[0]);
-            if (command){
-                if(command.usage){
-                    helpmsg = `\`${self.serverManager().prefix}${command.name} ${command.usage}\``
-                } else {
-                    helpmsg = `\`${self.serverManager().prefix}${command.name}\``;
+        self.db.getSettings(msg.guild.id, "prefix", (pref) => {
+            let prefix = pref;
+            if(prefix == "") prefix = self.serverManager().prefix;
+
+            if (msg.params.length >= 1){
+        		let helpmsg = "No command";
+                let command = msg.client.commands.get(msg.params[0]);
+                if (command){
+                    if(command.usage){
+                        helpmsg = `\`${prefix}${command.name} ${command.usage}\``
+                    } else {
+                        helpmsg = `\`${prefix}${command.name}\``;
+                    }
                 }
-            }
-    		message = self.createEmbed("info", helpmsg);
-    		self.send(msg, message);
-    	} else {
-    		message = self.createEmbed("info", "All available commands, more info !help <command>", "Commands", [
-    		{
-    			name: "Everyone",
-    			value: "!ping, !getroles, !kill, !help, !iam"
-    		},{
-    			name: "Music",
-    			value: "!play, !skip, !queue"
-    		},{
-    			name: "Cards Against Humanity",
-    			value: "!cstart, !cjoin, !cleave, !c, !cscoreboard, !creset",
-    		},{
-    			name: "Admin only",
-    			value: "!kick, !warn, !ban, !tempban, !unban, !setrole, !delrole,\n!set, !say, !silence, !unsilence, !see, !reload, !nuke\n!settings, !permissions"
-    		}
-    		]);
-    		self.send(msg, message);
-    	}
+        		message = self.createEmbed("info", helpmsg);
+        		self.send(msg, message);
+        	} else {
+        		message = self.createEmbed("info", "All available commands, more info " + prefix + "help <command>", "Commands", [
+        		{
+        			name: "Everyone",
+        			value: prefix + "ping, " + prefix + "getroles, " + prefix + "kill, " + prefix + "help, " + prefix + "iam"
+        		},{
+        			name: "Music",
+        			value: prefix + "play, " + prefix + "skip, " + prefix + "queue"
+        		},{
+        			name: "Cards Against Humanity",
+        			value: prefix + "cstart, " + prefix + "cjoin, " + prefix + "cleave, " + prefix + "c, " + prefix + "cscoreboard, " + prefix + "creset",
+        		},{
+        			name: "Admin only",
+        			value: prefix + "kick, " + prefix + "warn, " + prefix + "ban, " + prefix + "tempban, " + prefix + "unban, " + prefix + "setrole, " + prefix + "delrole,\n" + prefix + "set, " + prefix + "say, " + prefix + "silence, " + prefix + "unsilence, " + prefix + "see, " + prefix + "nuke\n" + prefix + "settings, " + prefix + "permissions"
+        		}
+        		]);
+        		self.send(msg, message);
+        	}
+        });
     }
 };
