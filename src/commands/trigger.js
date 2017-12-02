30c518591158b40a6be9c4f19fa822e27bb3d86c
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2017-12-02 02:20:49 +0100

    increased size

--- src/commands/trigger.js
+++ src/commands/trigger.js
@@ -10,7 +10,7 @@ module.exports = {
         if(msg.mentions.users.keyArray().length){
             avatarURL = msg.mentions.users.get(msg.mentions.users.firstKey()).avatarURL;
         }
-        require('child_process').exec("/root/repos/go/src/github.com/nomad-software/meme/meme -trigger -i " + avatarURL.replace("size=2048", "size=128") + " -cid 2874783bfa9be1e", function(error, stdout, stderr){
+        require('child_process').exec("/root/repos/go/src/github.com/nomad-software/meme/meme -trigger -i " + avatarURL.replace("size=2048", "size=256") + " -cid 2874783bfa9be1e", function(error, stdout, stderr){
             msg.channel.send(stdout);
         });
     }
