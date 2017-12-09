12d00426a96308e86368e2cbdddf408d9a764469
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2017-12-09 16:58:44 +0100

    messages seen added to botinfo

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -6,37 +6,38 @@ module.exports = {
     defaultPermission: 1,
     args: 0,
     execute(self, msg){
-        msg.client.fetchUser(botOwner).then( (user) => {
-            let members = 0;
-            for(guild of msg.client.guilds){
-                members+=guild[1].memberCount;
-            }
-        	self.send(msg, {
-                embed: {
-                    color: 0x5a00b1,
-                    title: "=-=-=-=-=-=-= Dupbot =-=-=-=-=-=-=",
-                    fields: [
-                        {
-                            name: "Guilds",
-                            value: msg.client.guilds.size
-                        }, {
-                            name: "Members",
-                            value: members
-                        }, {
-                            name: "Messages seen",
-                            value: "coming soon"
-                        }
-                    ],
-                    footer: {
-                        icon_url: user.avatarURL,
-                        text: "Made by " + user.username + "#" + user.discriminator
-                    },
-                    thumbnail: {
-                        url: msg.client.user.avatarURL
-                    }
+        self.db.getBotStats("messages", (messages) => {
+            msg.client.fetchUser(botOwner).then( (user) => {
+                let members = 0;
+                for(guild of msg.client.guilds){
+                    members+=guild[1].memberCount;
                 }
+                self.send(msg, {
+                    embed: {
+                        color: 0x5a00b1,
+                        title: "=-=-=-=-=-=-= Dupbot =-=-=-=-=-=-=",
+                        fields: [
+                            {
+                                name: "Guilds",
+                                value: msg.client.guilds.size
+                            }, {
+                                name: "Members",
+                                value: members
+                            }, {
+                                name: "Messages seen",
+                                value: messages
+                            }
+                        ],
+                        footer: {
+                            icon_url: user.avatarURL,
+                            text: "Made by " + user.username + "#" + user.discriminator
+                        },
+                        thumbnail: {
+                            url: msg.client.user.avatarURL
+                        }
+                    }
+                });
             });
-        })
-
+        });
     }
 };
