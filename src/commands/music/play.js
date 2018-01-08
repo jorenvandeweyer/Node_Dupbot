06f248ea82468c6a1c2a811ef87676490109bb8f
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-01-08 23:09:48 +0100

    music fix

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -6,6 +6,12 @@ module.exports = {
     guildOnly: true,
     execute(Client, msg){
         Client.db.getSettings(msg.guild.id, "voiceChannel", (channelId) => {
+            if(channelId.length){
+                channelId = channelId[0].value;
+            } else {
+                channelId = undefined;
+            }
+            
             let voiceChannelUser = msg.member.voiceChannelID;
             if(voiceChannelUser === undefined){
                 let message = Client.createEmbed("warn", "Go in a voiceChannel first");
