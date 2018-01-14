f657c279e325ceb46ea49f65ffb4cc96d20003d9
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-01-14 17:25:33 +0100

    added to DBL

--- src/utils/discordbots.js
+++ src/utils/discordbots.js
@@ -12,16 +12,16 @@ function set(Client){
         // console.log(`[+]${Client.bot.guilds.size} guilds connected.`);
     });
 
-    // Request.post(`https://discordbots.org/api/bots/382289192161640468/stats`, {
-    //     headers : {
-    //
-    //     },
-    //     json: true,
-    //     body: {"server_count": Client.bot.guilds.size}
-    // }, (err, res, body) => {
-    //     if(err) return console.error("[-]discordbotapi error?")
-    //     console.log(`[+]${Client.bot.guilds.size} guilds connected.`);
-    // });
+    Request.post(`https://discordbots.org/api/bots/${Client.bot.user.id}/stats`, {
+        headers : {
+            "Authorization" : Client.serverSettings.discordbotsapi2
+        },
+        json: true,
+        body: {"server_count": Client.bot.guilds.size}
+    }, (err, res, body) => {
+        if(err) return console.error("[-]discordbotapi error?")
+        // console.log(`[+]${Client.bot.guilds.size} guilds connected.`);
+    });
 }
 
 module.exports = {
